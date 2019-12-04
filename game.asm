98572e1ab713a9e1210305e1f9bfbf44f3ac5d97
Michał Budzyński <michal@virtualdesign.pl>
2019-12-04 01:57:00 +0100

    :open_hands: moving paddle

--- game.asm
+++ game.asm
@@ -3,20 +3,54 @@
 
 define paddle_x V0 ; Sprite X,Y position
 define paddle_y V1
+define old_paddle_x V2
+define old_paddle_y V3
+define step V6
 
-CLS
+; buttons
+define left V4
+define right V5
+
 
 ; Initial variable values
 LD  paddle_x, 24 ; screen is 64 pixels width,
                  ; paddle is 8 pixels long, so 24 is the center
 LD  paddle_x, 30 ; 32 - 2
 
+LD step, 1
+
+LD left, 5
+LD right, 6
+
+; clear the screen
+CLS
+
 ; The main loop
 loop:
+LD old_paddle_x, paddle_x
+LD old_paddle_y, paddle_y
+
+SKP left
+JP check_right
+
+SE paddle_x, 4
+SUB paddle_x, step
+
+check_right:
+SKP right
+JP draw
+
+SE paddle_x, 56
+ADD paddle_x, 1
+
+draw:
 LD  I, paddle
 
-DRW paddle_x, paddle_x, 8
-	JP  loop
+DRW old_paddle_x, old_paddle_y, 8
+DRW paddle_x, paddle_y, 8
+
+; end of the loop
+JP  loop
 
 paddle:
 db  %01111110,
