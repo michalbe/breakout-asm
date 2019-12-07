4d2345c20b3fd41f4b2709c22f91eb8d45b679dd
Michał Budzyński <michal@virtualdesign.pl>
2019-12-07 02:03:00 +0100

    :crown: Moving paddle

--- game.asm
+++ game.asm
@@ -13,9 +13,9 @@ define right V5
 
 
 ; Initial variable values
-LD  paddle_x, 24 ; screen is 64 pixels width,
-                 ; paddle is 8 pixels long, so 24 is the center
-LD  paddle_x, 30 ; 32 - 2
+LD  paddle_x, 26 ; screen is 64 pixels width,
+                 ; paddle is 8 pixels long, so 26 is the center
+LD  paddle_y, 30 ; 32 - 2
 
 LD step, 1
 
@@ -27,13 +27,14 @@ CLS
 
 ; The main loop
 loop:
+CLS
 LD old_paddle_x, paddle_x
 LD old_paddle_y, paddle_y
 
 SKP left
 JP check_right
 
-SE paddle_x, 4
+SE paddle_x, 0
 SUB paddle_x, step
 
 check_right:
@@ -46,7 +47,6 @@ ADD paddle_x, 1
 draw:
 LD  I, paddle
 
-DRW old_paddle_x, old_paddle_y, 8
 DRW paddle_x, paddle_y, 8
 
 ; end of the loop
